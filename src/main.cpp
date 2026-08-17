f3dc797024d0e394c26fafda42c4cf5c1d7c3ac2
Mahad Khan <mahadunaiskhan@gmail.com>
2026-08-17 11:41:01 +0500

    added SPI screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <TFT_eSPI.h>
 
 // Data wire is connected to GPIO 16 (RX2)
 #define ONE_WIRE_BUS 16
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+TFT_eSPI tft = TFT_eSPI();
 
 unsigned long lastRead = 0;
 const unsigned long READ_INTERVAL = 2000;
@@ -15,6 +17,7 @@ float currentTemp = DEVICE_DISCONNECTED_C;
 
 void sendDataSerial(float temp);
 void readSensor();
+void updateDisplay();
 
 void setup() {
   Serial.begin(115200);
@@ -27,11 +30,22 @@ void setup() {
   
   // init ds18b20
   sensors.begin();
+
+  // init display
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextSize(3);
+
+  updateDisplay();
 }
 
 void loop() {
-  readSensor();
-  delay(READ_INTERVAL);
+  // Enforce non-blocking interval
+  if (millis() - lastRead > READ_INTERVAL) {
+    readSensor();
+  }
 }
 
 void readSensor(){
@@ -44,11 +58,30 @@ void readSensor(){
 
   if (t != DEVICE_DISCONNECTED_C){
     sendDataSerial(t);
-  
   }
+
+  updateDisplay();
 }
 
 void sendDataSerial(float temp){
   Serial.print("DATA,");
   Serial.println(temp);
+}
+
+void updateDisplay(){
+  tft.setCursor(0, 0);
+  
+  tft.println("--- Heat System ---");
+  tft.println();
+  
+  tft.print("Temp: ");
+  if (currentTemp == DEVICE_DISCONNECTED_C) {
+    tft.println("Error       "); 
+  } else {
+    tft.print(currentTemp);
+    tft.println(" C       "); 
+  }
+  
+  tft.println();
+  tft.println("Logging over USB");
 }
